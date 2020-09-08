ec9a0caec7c8e4ac085eb01658b5e921f0c38218
hamahormz <70761216+hamahormz@users.noreply.github.com>
2020-09-08 15:16:09 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,4 @@
-const http = require("http");
-const express = require("express");
-const app = express();
-app.get("/", (request, response) => {
-response.sendStatus(200);
-});
-app.listen(process.env.PORT);
-setInterval(() => {
-http.get(`http://spam-1-20.glitch.me/`);
-}, 280000);
+
 
 
 
